c283d2f2f4761bd6357cf1c452bfcd2c2bb3901e
Suryan Jain <suryanjain14@gmail.com>
2020-07-28 20:03:26 +0530

    added login logout functions added requirements file

--- blogflask/router.py
+++ blogflask/router.py
@@ -1,9 +1,9 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for,request
 from blogflask import app
 from blogflask.forms import *
-from blogflask import db,bcrypt
+from blogflask import db, bcrypt
 from blogflask.models import User
-
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
     {
@@ -34,24 +34,49 @@ def about():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('You have been logged in!', 'success')
-        return redirect(url_for('home'))
-    else:
-        flash('Login Unsuccessful. Please check username and password', 'danger')
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.passwords, form.password.data):
+                login_user(user, remember=form.remember.data)
+                flash('You have been logged in!', 'success')
+                next_page=request.args.get('next')
+                return redirect(next_page) if next_page else redirect(url_for('home'))
+            else:
+                flash(' Incorrect username and password', 'danger')
+        else:
+            flash(' Incorrect username and password', 'danger')
+
+
     return render_template('login.html', title='Login', form=form)
 
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data,email=form.email.data,passwords=hashed_password)
+        user = User(username=form.username.data, email=form.email.data, passwords=hashed_password)
         db.session.add(user)
         db.session.commit()
         flash(f'{form.username.data} your account has been created', 'success')
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html', title='Account')
