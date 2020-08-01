f7aeab44c6fad822232995adfee67dfec98c5850
suryan14 <suryanjain14@gmail.com>
2020-08-01 20:43:18 +0530

    added posts functionality crete edit delete post

--- blogflask/forms.py
+++ blogflask/forms.py
@@ -1,5 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField,FileField
+from flask_wtf.file import file_allowed, FileField
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, equal_to, ValidationError
 from blogflask.models import User
 
@@ -29,19 +31,32 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 
-class AccountUpdate(FlaskForm):
+class AccountUpdateForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    image = FileField()
-    submit = SubmitField('Sign Up')
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[file_allowed(['jpg', 'png', 'gif'])])
+    submit = SubmitField('Update')
 
-    @staticmethod
-    def validate_username(username):
-        user = User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError('Username already taken')
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Username already taken')
 
     def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user:
-            raise ValidationError('Email already taken')
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email already taken')
 
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired(), Length(min=3, max=250)])
+    submit = SubmitField('Post')
+
+
+class PostEdit(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired(), Length(min=3, max=250)])
+    submit = SubmitField('Update')
--- blogflask/router.py
+++ blogflask/router.py
@@ -1,30 +1,19 @@
-from flask import render_template, flash, redirect, url_for,request
+from flask import render_template, flash, redirect, url_for, request
 from blogflask import app
 from blogflask.forms import *
 from blogflask import db, bcrypt
-from blogflask.models import User
+from blogflask.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-
-posts = [
-    {
-        'author': 'suryan jain',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'hkdashk'
-    },
-    {
-        'author': 'jain suryan',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'kabhi to ki thi'
-    }
-]
+from PIL import Image
+import secrets
+import os
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template("home.html", posts=posts, title='Home')
+    posts = Post.query.all()
+    return render_template("home.html", posts=posts, title='Home', user=User())
 
 
 @app.route("/about")
@@ -43,13 +32,12 @@ def login():
             if bcrypt.check_password_hash(user.passwords, form.password.data):
                 login_user(user, remember=form.remember.data)
                 flash('You have been logged in!', 'success')
-                next_page=request.args.get('next')
+                next_page = request.args.get('next')
                 return redirect(next_page) if next_page else redirect(url_for('home'))
             else:
                 flash(' Incorrect username and password', 'danger')
         else:
             flash(' Incorrect username and password', 'danger')
-
 
     return render_template('login.html', title='Login', form=form)
 
@@ -75,22 +63,91 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route('/account')
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    i = Image.open(form_picture)
+    i.thumbnail((125, 125))
+    i.save(picture_path)
+    return picture_fn
+
+
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html', title='Account',image_file=image_file)
+    form = AccountUpdateForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash(f'{form.username.data} your account has been updated', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
 
 
-@app.route('/update')
+@app.route('/post/new', methods=['GET', 'POST'])
 @login_required
-def account_update():
-    if current_user.is_authenticated:
-        form = RegistrationForm()
-        if form.validate_on_submit():
-            user = User.query.filter_by(email=current_user.email)
+def new_posts():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, user_id=current_user.id, content=form.content.data)
+        db.session.add(post)
+        db.session.commit()
+        flash(f'{current_user.username} Content Posted', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='Posts', form=form)
 
-            db.session.add(user)
+
+@app.route('/posts/<user_id>')
+def user_posts(user_id):
+    user = User.query.get(user_id)
+    posts = Post.query.filter_by(user_id=user_id).all()
+    return render_template("user_posts.html", posts=posts, title=current_user.username + ' Posts', user=user)
+
+
+@app.route('/post/edit/<int:post_id>', methods=['GET', 'POST'])
+def post_edit(post_id):
+    post = Post.query.get(post_id)
+    try:
+        if post.user_id == current_user.id:
+            form = PostEdit()
+            if form.validate_on_submit():
+                post.title = form.title.data
+                post.content = form.content.data
+                db.session.commit()
+
+                flash(f'{post.title} has been updated', 'success')
+                return redirect(url_for('user_posts', user_id=current_user.id))
+            elif request.method == 'GET':
+                form.title.data = post.title
+                form.content.data = post.content
+            return render_template("edit_post.html", post=post, form=form)
+        else:
+            return redirect(url_for('home'))
+    except:
+        flash(f'ACCESS DENIED', 'danger')
+        return redirect(url_for('home'))
+
+
+@app.route('/post/delete/<int:post_id>')
+def delete_post(post_id):
+    post = Post.query.get(post_id)
+    try:
+        if post.user_id == current_user.id:
+            db.session.delete(post)
             db.session.commit()
-            flash(f'{form.username.data} your account has been created', 'success')
-    return url_for('account')
+            return redirect(url_for('user_posts', user_id=current_user.id))
+        else:
+            return redirect(url_for('home'))
+    except:
+        flash(f'ACCESS DENIED', 'danger')
+        return redirect(url_for('home'))
